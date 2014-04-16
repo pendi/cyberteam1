95331bdd2f345d45bb87f8979fe283f49293b32c
Wahyu Taufik <wahyutaufik37@gmail.com>
2014-04-16 10:59:02 +0700

    initial

--- application/default/modules/web/views/web/cat_list.php
+++ application/default/modules/web/views/web/cat_list.php
@@ -8,11 +8,11 @@
                 <ul class="flat">
                 <?php foreach ($film as $item):?>
                     <li>
-                        <a href="#">
+                        <a href="<?php echo site_url('web/detail_film/'.$item['id'])?>">
                             <div class="image" style="background: url(<?php echo base_url('data/').'/'.$item['cover'] ?>) center no-repeat; background-size: cover;"></div>
                         </a>
                         <p class="desc">
-                            <a href="#"><?php echo $item['title']?></a>
+                            <a href="<?php echo site_url('web/detail_film/'.$item['id'])?>"><?php echo $item['title']?></a>
                         </p>
                     </li>
 				<?php endforeach;?>
--- application/default/modules/web/views/web/list_movie.php
+++ application/default/modules/web/views/web/list_movie.php
@@ -28,70 +28,14 @@
                 <ul class="flat">
                 <?php foreach ($film as $item):?>
                     <li>
-                        <a href="#">
+                        <a href="<?php echo site_url('web/detail_film/'.$item['id'])?>">
                             <div class="image" style="background: url(<?php echo base_url('data/').'/'.$item['cover'] ?>) center no-repeat; background-size: cover;"></div>
                         </a>
                         <p class="desc">
-                            <a href="#"><?php echo $item['title']?></a>
+                            <a href="<?php echo site_url('web/detail_film/'.$item['id'])?>"><?php echo $item['title']?></a>
                         </p>
                     </li>
 				<?php endforeach;?>
-                    <!-- <li>
-                        <a href="#">
-                            <div class="image" style="background: url(themes/img/apocalypse.jpg) center no-repeat; background-size: cover;"></div>
-                        </a>
-                        <p class="desc">
-                            <a href="#">Apocalypse Pompeii</a>
-                        </p>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div class="image" style="background: url(themes/img/arena.jpg) center no-repeat; background-size: cover;"></div>
-                        </a>
-                        <p class="desc">
-                            <a href="#">Arena</a>
-                        </p>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div class="image" style="background: url(themes/img/captainphillips.jpg) center no-repeat; background-size: cover;"></div>
-                        </a>
-                        <p class="desc">
-                            <a href="#">Captain Phillips</a>
-                        </p>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div class="image" style="background: url(themes/img/lankwaifong2.jpg) center no-repeat; background-size: cover;"></div>
-                        </a>
-                        <p class="desc">
-                            <a href="#">Lan Kwai Fong 2</a>
-                        </p>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div class="image" style="background: url(themes/img/lankwaifong3.jpg) center no-repeat; background-size: cover;"></div>
-                        </a>
-                        <p class="desc">
-                            <a href="#">Lan Kwai Fong 3</a>
-                        </p>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div class="image" style="background: url(themes/img/princeofpersia.jpg) center no-repeat; background-size: cover;"></div>
-                        </a>
-                        <p class="desc">
-                            <a href="#">Prince of Persia</a>
-                        </p>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <div class="image" style="background: url(themes/img/hercules.jpg) center no-repeat; background-size: cover;"></div>
-                        </a>
-                        <p class="desc">
-                            <a href="#">The Legend Of Hercules</a>
-                        </p>
-                    </li> -->
                 </ul>
                 <div class="more">
                     <a class="pull-right" href="#">Show All Movies</a>
